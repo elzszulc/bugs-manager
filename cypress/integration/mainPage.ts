fa8073eb9a9fda169c709f5791504802a2d2fcf4
Elżbieta Szulc <elszulc@wp.pl>
2021-07-23 12:54:59 +0200

    Add tiny fix to mainPage.ts

--- cypress/integration/mainPage.ts
+++ cypress/integration/mainPage.ts
@@ -4,7 +4,7 @@ import { BugDetails, PaginationDetails } from "../support/fixtures"
 describe('Main page of bug manager', () => {
     beforeEach(() => {
         visitingMainPage()
-      })
+    })
 
     context('When user has an access to Bug Manager', () => {
         it('should be able to add a bug', () => {
